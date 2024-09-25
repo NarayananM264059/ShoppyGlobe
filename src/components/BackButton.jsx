209b1e7b332dee1ff407abe9a6319c17d48ca3ae
Narayanan.M <123665584+NarayananM264059@users.noreply.github.com>
2024-09-25 23:51:39 +0530

    final sumits

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,8 +1,7 @@
-
 const BackButton = () => {
   return (
     <button
-      className="mb-4 flex items-center text-green-400 hover:underline transition duration-300 ease-in-out"
+      className="mb-4 flex items-center text-green-400 hover:bg-green-400 hover:text-white border border-green-400 rounded-lg px-4 py-2 transition duration-300 ease-in-out shadow-lg"
       onClick={() => window.history.back()}
     >
       <span className="mr-2">
