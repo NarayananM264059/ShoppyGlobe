9b91ca69d2e30dee6406835cb5f736559c2c28e2
Narayanan.M <123665584+NarayananM264059@users.noreply.github.com>
2024-09-26 00:24:02 +0530

    sub

--- src/components/headerComponents/Header.jsx
+++ src/components/headerComponents/Header.jsx
@@ -4,13 +4,13 @@ import { useSelector } from "react-redux";
 import { selectCartItems } from "../../redux/selectors/cartSelectors";
 
 const Header = () => {
-  const totalCarts = useSelector(selectCartItems); // Get cart items from redux store
+  const totalCarts = useSelector(selectCartItems); 
 
   return (
     <header className="flex items-center justify-between py-4 bg-gradient-to-r from-purple-500 to-pink-500 sticky top-0 shadow-md px-8">
       <div className="flex items-center">
-        <Logo /> {/* Logo component */}
-        <h1 className="text-white text-2xl font-bold ml-4">ShopEase</h1> {/* App title */}
+        <Logo /> 
+        <h1 className="text-white text-2xl font-bold ml-4">ShopEase</h1>
       </div>
 
       {/* Cart icon with item count */}
