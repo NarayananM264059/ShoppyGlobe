a7c8c8ae94e3a9e8291f079a7bad0197b7d09278
Narayanan.M <123665584+NarayananM264059@users.noreply.github.com>
2024-09-26 00:38:57 +0530

    Update Logo.jsx

--- src/components/headerComponents/Logo.jsx
+++ src/components/headerComponents/Logo.jsx
@@ -1,12 +1,11 @@
 import { Link } from "react-router-dom";
-// Import the SVG logo
-import logo from "../../assets/logo.svg"; 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
+import { faShoppingBag } from "@fortawesome/free-solid-svg-icons"; 
 
 const Logo = () => {
   return (
     <Link to={"/home"} className="flex items-center space-x-3 transition-transform duration-300 hover:scale-105">
-      {/* Use the imported SVG logo */}
-      <img src={logo} alt="ShoppyGlobe Logo" className="h-12" />
+      <FontAwesomeIcon icon={faShoppingBag} className="text-white text-4xl transform transition-transform duration-300 hover:rotate-12" />
       <span className="text-white text-4xl font-extrabold tracking-wider shadow-md hover:underline">
         ShoppyGlobe
       </span>
